a96a4de714b31a8d9ff17fbcfcd38abe26ed008d
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-14 12:00:52 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        char c = 'A';
+        String fullName = "Natnael Tessema";
+        String[] fruit = {"Apple", "Banana", "Cherry"};
+        Date d = new Date(0);
+        System.out.println(c);
+        System.out.println(fullName);
+       // System.out.println(fruit[0] + fruit[1] + fruit[2]);
+        System.out.println(d);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < fruit.length; i++){
+            System.out.println(i + " " + fruit[i]);
         }
+        System.out.println("ALL DONE!");
     }
 }
